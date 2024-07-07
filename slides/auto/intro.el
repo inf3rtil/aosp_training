0f58adbab8f1016a83f04ca074308fe5b99a8bec
Fernando Ferreira Silva <fernando@fer.dev.br>
2024-07-07 11:38:50 -0300

    add first slides

---
+++ slides/auto/intro.el
@@ -0,0 +1,19 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "intro"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"))
+ :latex)
+
